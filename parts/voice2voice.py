9babfcd88d0451455c2a452376c6754f1e114167
iTzgon <87726082+iTzgon@users.noreply.github.com>
2024-10-16 22:09:12 -0300

    Update voice2voice.py
    traduccion gradio

--- parts/voice2voice.py
+++ parts/voice2voice.py
@@ -20,7 +20,7 @@ with gr.Tab(i18n("Translate")):
                         label=i18n("Path to folder with audio files (High priority)"), value=None)
             with gr.Column():
                 translate_whisper_model = gr.Dropdown(label=i18n("Whisper Model"), choices=[
-                                                      "small", "medium", "large-v2", "large-v3"], value="medium")
+                                                      "small", "medium", "large-v2", "large-v3"], value="large-v3")
                 translate_audio_mode = gr.Radio(label=i18n("Mode"), choices=[
                                                 1, 2,3], value=2, info=i18n("Mode Desk"))
                 translate_translator = gr.Radio(label=i18n("Translator"), choices=[
@@ -29,16 +29,16 @@ with gr.Tab(i18n("Translate")):
                 deepl_auth_key_textbox = gr.Textbox(label="Deepl API Key", value="",type="password",visible=False)
                 
                 with gr.Row():
-                    translate_num_sent = gr.Slider(label=i18n("Number of sentences that will be voiced at one time"),minimum=1,maximum=6,step=1,visible=True)
-                    translate_max_reference_seconds= gr.Slider(label=i18n("Number of reference seconds that will be used"),minimum=10,maximum=600,value=20,step=1,visible=True)
+                    translate_num_sent = gr.Slider(label=i18n("Número de oraciones que se expresarán a la vez"),minimum=1,maximum=6,step=1,visible=True)
+                    translate_max_reference_seconds= gr.Slider(label=i18n("Número de segundos de referencia que se utilizarán"),minimum=10,maximum=600,value=20,step=1,visible=True)
                 with gr.Row():
-                    sync_with_original_checkbox = gr.Checkbox(label=i18n("Sync timing with original ( Accuracy ~ 80 - 90% )"), value=False)
-                with gr.Accordion(label=i18n("Subtitle settings")):
+                    sync_with_original_checkbox = gr.Checkbox(label=i18n("Sincronización de tiempo con el original (precisión ~ 80 - 90%)"), value=False)
+                with gr.Accordion(label=i18n("Configuración de subtítulos")):
                     with gr.Row():
-                        max_width_sub_v2v = gr.Slider(label=i18n("Max width per line"), minimum=1, maximum=100, value=40, step=1)
-                        max_line_sub_v2v = gr.Slider(label=i18n("Max line"),visible=True, minimum=1, maximum=20, value=2, step=1)
+                        max_width_sub_v2v = gr.Slider(label=i18n("Ancho máximo por línea"), minimum=1, maximum=100, value=40, step=1)
+                        max_line_sub_v2v = gr.Slider(label=i18n("Línea máxima"),visible=True, minimum=1, maximum=20, value=2, step=1)
                     with gr.Row():
-                        highlight_words_v2v = gr.Checkbox(label=i18n("Highlight Words"),visible=False, value=False)
+                        highlight_words_v2v = gr.Checkbox(label=i18n("Resaltar palabras"),visible=False, value=False)
                 # speakers_list = XTTS.get_speakers()
                 # speaker_value = ""
                 # if not speakers_list:
@@ -70,20 +70,20 @@ with gr.Tab(i18n("Translate")):
                 
                 with gr.Row():
                     translate_source_lang = gr.Text(
-                        value="auto", label=i18n("Enter lang code of source lang, if you want define automaticly type 'auto'"))
+                        value="auto", label=i18n("Ingrese el código de idioma del idioma de origen, si desea definirlo automáticamente, escriba 'auto'"))
                     translate_target_lang = gr.Dropdown(
-                        label=i18n("Target lang"), choices=supported_languages_list, value="ru")
+                        label=i18n("Idioma de destino"), choices=supported_languages_list, value="ru")
                     translate_speaker_lang = gr.Dropdown(
-                        label=i18n("Speaker Accent"), choices=supported_languages_list, value="ru")
+                        label=i18n("Acento del Speaker "), choices=supported_languages_list, value="ru")
                 with gr.Column():
                     with gr.Accordion("Whisper settings",open=False):
                         with gr.Row():
                             # translate_whisper_model = gr.Dropdown(label=i18n("Whisper Model"), choices=["small", "medium", "large-v2", "large-v3"], value="medium")
-                            translate_whisper_compute_time = gr.Radio(label="Compute Type",choices=["int8","float16"],value="float16",info="change to 'int8' if low on GPU mem (may reduce accuracy)")
-                            translate_whisper_device = gr.Radio(label="Device",choices=["cuda","cpu"],value="cuda",info="change to 'int8' if low on GPU mem (may reduce accuracy)")
+                            translate_whisper_compute_time = gr.Radio(label="Compute Type",choices=["int8","float16"],value="float16",info="Cambie a 'int8' si tiene poca memoria en la GPU (puede reducir la precisión)")
+                            translate_whisper_device = gr.Radio(label="Device",choices=["cuda","cpu"],value="cuda",info="Cambie a 'CPU' si no tiene GPU con CUDA (puede reducir la precisión)")
                         with gr.Row():
-                            translate_whisper_batch_size = gr.Slider(label="Batch Size", minimum=1, maximum=32, value=8, step=1,info="reduce if low on GPU mem")
-                            translate_whisper_aline = gr.Checkbox(value=True,label="Align whisper output")
+                            translate_whisper_batch_size = gr.Slider(label="Batch Size", minimum=1, maximum=32, value=8, step=1,info="reducir si hay poca memoria")
+                            translate_whisper_aline = gr.Checkbox(value=True,label="Alinear la salida del whisper")
                 with gr.Column():
                   with gr.Accordion(i18n("XTTS settings"), open=False,visible=True):
                     translate_speed = gr.Slider(
@@ -129,26 +129,26 @@ with gr.Tab(i18n("Translate")):
                         value=0.85,
                     )
                     translate_sentence_split = gr.Checkbox(
-                        label=i18n("Enable text splitting"),
+                        label=i18n("Habilitar división de texto"),
                         value=False,
                     )
                     
         with gr.Column():
             translate_status_bar = gr.Label(
-                value=i18n("Select target language, mode and upload audio then press translate button."))
+                value=i18n("Seleccione el idioma de destino, el modo y cargue el audio, luego presione el botón traducir."))
             translate_video_output = gr.Video(
                 label=i18n("Mp4 Translate"), value=None, interactive=False,visible=False)
             translate_voice_output = gr.Audio(
                 label=i18n("Result"), value=None, interactive=False)
             translate_files_output = gr.Files(label=i18n("Subtitles"),interactive=False)
-            with gr.Accordion(i18n("Translate mode")):
-              with gr.Tab(i18n("Simple mode")):
+            with gr.Accordion(i18n("Modo traducir")):
+              with gr.Tab(i18n("Modo simple")):
                 translate_btn = gr.Button(value=i18n("Translate"))
-              with gr.Tab(i18n("Advanced mode")):
-                translate_advance_stage1_btn = gr.Button(value=i18n("Step 1 - Transcribe, translate and edit the text"))
-                translate_advance_stage1_text = gr.TextArea(label=i18n("Translated Text"),value=None,visible=False)
-                translate_advance_stage2_btn = gr.Button(value=i18n("Stage 2 - Voice Text"),visible=False)
-                transalte_advance_markdown = gr.Markdown(i18n("Work in progress..."),visible=False)
+              with gr.Tab(i18n("Modo avanzado")):
+                translate_advance_stage1_btn = gr.Button(value=i18n("Paso 1 – Transcribir, traducir y editar el texto"))
+                translate_advance_stage1_text = gr.TextArea(label=i18n("Texto traducido"),value=None,visible=False)
+                translate_advance_stage2_btn = gr.Button(value=i18n("Etapa 2 - Texto de voz"),visible=False)
+                transalte_advance_markdown = gr.Markdown(i18n("Trabajo en progreso..."),visible=False)
 
 if RVC_ENABLE:
   with gr.Tab("RVC"):
